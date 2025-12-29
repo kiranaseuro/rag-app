207a82263a92d8e99b2652c2282f8458a64f247e
kiranaseuro <kiran.kumar@aseuro.in>
2025-12-29 11:49:47 +0530

    Fix: Add missing private_subnet_cidrs to rag-with-eval use case

--- use_cases/rag-with-eval/variables.tf
+++ use_cases/rag-with-eval/variables.tf
@@ -29,6 +29,11 @@ variable "public_subnet_cidrs" {
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
 variable "allowed_cidrs" {
   type    = list(string)
   default = ["10.0.0.0/8"]
